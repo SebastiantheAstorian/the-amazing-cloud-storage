2adee5521b984af4a02c2231c849911a3db7611c
Christina Pacher <a01406933@unet.univie.ac.at>
2020-01-20 11:38:50 +0100

    created manager application

---
+++ cloud-storage-manager/src/test/java/com/cc/cloudstoragemanager/CloudStorageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cc.cloudstoragemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStorageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
