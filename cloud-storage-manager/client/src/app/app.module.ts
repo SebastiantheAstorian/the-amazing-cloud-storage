29f437d5a666d5e71215e5ca89f6c74b2425b4f7
DESKTOP-PC\Sebastian Komposch <sebastian.komposch@gmail.com>
2020-01-21 18:28:29 +0100

    defined client style and base layout

--- cloud-storage-manager/client/src/app/app.module.ts
+++ cloud-storage-manager/client/src/app/app.module.ts
@@ -1,14 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule, MatTabsModule, MatToolbarModule} from "@angular/material";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
