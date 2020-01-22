5cc5b0fcf48e2be6dd2cf578d4d1d369b27eea0c
Christina Pacher <a01406933@unet.univie.ac.at>
2020-01-22 17:24:56 +0100

    added logging output

--- cloud-storage-manager/src/main/java/com/cc/cloudstoragemanager/CloudStorageManagerService.java
+++ cloud-storage-manager/src/main/java/com/cc/cloudstoragemanager/CloudStorageManagerService.java
@@ -1,16 +1,15 @@
 package com.cc.cloudstoragemanager;
 
 import com.cc.cloudstoragemanager.model.ValueObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@Slf4j
 @RestController
-public class ManagerService {
-
-
+public class CloudStorageManagerService {
 
 
 
@@ -18,6 +17,8 @@ public class ManagerService {
     @GetMapping("/values")
     public int addValue(@RequestBody ValueObject val){
         int key = val.getKey();
+
+        log.info("Adding new value with key " + key);
 
         //hash the key
         //get the IP address of the node associated with the hash
@@ -29,6 +30,8 @@ public class ManagerService {
     //delete
     @DeleteMapping("/values/{key}")
     public int deleteValue(@PathVariable int key){
+        log.info("Deleting value with key " + key);
+
         //hash the key
         //get the IP address of the node associated with the hash
         //send the delete request to the node
@@ -41,12 +44,16 @@ public class ManagerService {
     //query key
     @GetMapping("/values/{key}")
     public ValueObject queryValueByKey(@PathVariable int key){
+        log.info("Querying value with key " + key);
+
         return null;
     }
 
     //range query
     @GetMapping(value="/values", params = {"from", "to"})
     public List<ValueObject> queryValuesInRange(@RequestParam int from, @RequestParam int to){
+        log.info("Querying values between keys " + from + " and " + to);
+
         return null;
     }
 
