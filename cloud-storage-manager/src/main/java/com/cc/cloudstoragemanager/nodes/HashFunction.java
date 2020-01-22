40408f7efeeac6ff78b9bcc7ef1cf2fc051dac4f
Christina Pacher <a01406933@unet.univie.ac.at>
2020-01-22 18:33:07 +0100

    added method to query all addresses for a range of keys

--- cloud-storage-manager/src/main/java/com/cc/cloudstoragemanager/nodes/HashFunction.java
+++ cloud-storage-manager/src/main/java/com/cc/cloudstoragemanager/nodes/HashFunction.java
@@ -1,7 +1,12 @@
 package com.cc.cloudstoragemanager.nodes;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
 public class HashFunction {
 
     @Value("${nodes.addresses}")
@@ -9,12 +14,31 @@ public class HashFunction {
 
     private int getHashValue(int key){
         int n = nodeAddresses.length;
-        return key % n;
+        int hash = key % n;
+
+        //TODO: check that hash is >= 0
+
+        return hash;
     }
 
     public String getAddressByKey(int key){
         int hash = getHashValue(key);
         String address = nodeAddresses[hash];
         return address;
+    }
+
+    public Set<String> getAddressesInRange(int key1, int key2) {
+        if (key2 <= key1) throw new IllegalArgumentException("Invalid range: key2 must be greater than key1");
+
+        //using a set to make sure each address is only contained once
+        Set<String> addresses = new HashSet<>();
+
+        //TODO: assumption: both key1 and key2 are included in the range - is that correct? (Assignment does not specify this)
+        for (int currentKey = key1; currentKey <= key2; ++currentKey) {
+            String currentAddress = getAddressByKey(currentKey);
+            addresses.add(currentAddress);
+        }
+
+        return addresses;
     }
 }
