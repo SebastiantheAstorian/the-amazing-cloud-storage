69a4ccea86020a650a30bb52d84f7929316d5230
DESKTOP-PC\Sebastian Komposch <sebastian.komposch@gmail.com>
2020-01-23 15:49:10 +0100

    Revert "Merge branch 'node-implementation'"
    This reverts commit b2fd366f4bf5a4c783b88451aca76a77dc009155, reversing
    changes made to 2ad4ac7f5e3a3d15e30047369e80d7cd7964baed.

--- cloud-storage-node/src/main/java/cc/ass5/storagenodes/cloudstoragenode/errorhandling/ValueObjectAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package cc.ass5.storagenodes.cloudstoragenode.errorhandling;
-
-public class ValueObjectAlreadyExistException extends RuntimeException {
-    public ValueObjectAlreadyExistException(int key) {
-        super("ValueObject with key  " + key + " already exists");
-    }
-}
--- cloud-storage-node/src/main/java/cc/ass5/storagenodes/cloudstoragenode/errorhandling/ValueObjectNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package cc.ass5.storagenodes.cloudstoragenode.errorhandling;
-
-public class ValueObjectNotAvailableException extends RuntimeException {
-    public ValueObjectNotAvailableException(int key) {
-        super("ValueObject with key  " + key + " not available");
-    }
-}
